9f41fd680f5bbba1e7828634bcc4eadf9e4f31cb
Nathan <nathanpatrick939@gmail.com>
2020-11-18 21:06:49 +0000

    text counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,19 @@
-console.log("hello")$(document).ready(function() {
-  console.log("this is working")
+$(document).ready(function() {
+ $(".text-box").on("keyup", function() {
+  let count = ($(this).val().length ) 
+  const $tweetForm = $(this).closest("form").html()
+  const $counter= $(this).next().children(".counter")
+  
+  $counter.html(140 - count)
+  if ($counter.html() < 0){
+    $counter.css("color", "red")
+  }else{
+    $counter.css("color", "#545149")
+  }
+  
+   console.log(`key went down ${count} `)
+   console.log("this is tweetForm", $tweetForm)
+   console.log("this is counter", $counter);
+ })
 });
+
